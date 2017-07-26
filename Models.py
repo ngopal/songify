e43df658deba477207e68888eb460bfc25c42514
ngopal <nikhilgopal@gmail.com>
2017-07-26 10:41:05 -0700

    edited models to be per playlist

--- Models.py
+++ Models.py
@@ -5,11 +5,12 @@ import operator
 
 class DocModel:
     """A simple document model using cosine similarity"""
-    def __init__(self):
+    def __init__(self, pl_name):
+        self.pl_name = pl_name
         self.helper = SQL()
-
-    def nullModel(self, image_keywords, pl_name):
         self.pdf = pd.read_sql_table(pl_name, self.helper.engine)
+
+    def nullModel(self, image_keywords):
         # create models
         doc_models = []
         nlp = spacy.load('en')
@@ -35,9 +36,9 @@ class DocModel:
             print(k, v)
 
 if __name__ == "__main__":
-    Model = DocModel()
+    Model = DocModel("4SMubSJhL8oHG1RNa6RGkQ") # Can make Models per playlist
     keyws = ['n04162706 seat belt, seatbelt', 'n02965783 car mirror', 'n03452741 grand piano, grand', 'n04070727 refrigerator, icebox', 'n04162706 seat belt, seatbelt', 'n04200800 shoe shop, shoe-shop, shoe store', 'n03670208 limousine, limo', 'n02687172 aircraft carrier, carrier, flattop, attack aircraft carrier', 'n04356056 sunglasses, dark glasses, shades', 'n04356056 sunglasses, dark glasses, shades', 'n03534580 hoopskirt, crinoline', 'n03032252 cinema, movie theater, movie theatre, movie house, picture palace']
     # spotifyplaylistid, 4SMubSJhL8oHG1RNa6RGkQ
-    Model.nullModel(keyws, "4SMubSJhL8oHG1RNa6RGkQ")
+    Model.nullModel(keyws)
 
 
