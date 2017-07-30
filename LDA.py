f689d9503ffbeb2185715098d366fc3055ff7f6e
ngopal <nikhilgopal@gmail.com>
2017-07-29 18:21:46 -0700

    added text processing steps to LDA.py

--- LDA.py
+++ LDA.py
@@ -8,37 +8,60 @@ import sys
 sys.path.append('/Users/nikhilgopal/Documents/Insight/vid2song/')
 from Helpers.SQLhelper import SQL
 import pandas as pd
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+import spacy
+from nltk.corpus import words
 
 # Load Database
 DBhelper = SQL()
 
-# all_tables = ["19PgP2QSGPcm6Ve8VhbtpG", "37i9dQZF1DX1ewVhAJ17m4", "37i9dQZF1DX5bjCEbRU4SJ","3vxotOnOGDlZXyzJPLFnm2","49oW3sCI91kB2YGw7hsbBv","4tZSI7b1rnGVMdkGeIbCI4","68bXT1MZWZvLOJc0FZrgf7","6wz8ygUKjoHfbU7tB9djeS","7eHApqa9YVkuO6gELsju2j"]
-all_tables = ["19PgP2QSGPcm6Ve8VhbtpG", "37i9dQZF1DX1ewVhAJ17m4"]
+all_tables = ["19PgP2QSGPcm6Ve8VhbtpG", "37i9dQZF1DX1ewVhAJ17m4", "37i9dQZF1DX5bjCEbRU4SJ","3vxotOnOGDlZXyzJPLFnm2","49oW3sCI91kB2YGw7hsbBv","4tZSI7b1rnGVMdkGeIbCI4","68bXT1MZWZvLOJc0FZrgf7","6wz8ygUKjoHfbU7tB9djeS","7eHApqa9YVkuO6gELsju2j"]
+# all_tables = ["19PgP2QSGPcm6Ve8VhbtpG", "37i9dQZF1DX1ewVhAJ17m4"]
 
+lyrics = []
 
-def multipleTableNames(list_of_tables):
-    qc = "SELECT \'lyrics\' FROM "
-    for k in list_of_tables:
-        qc += "\"" + k + "\", "
-    # qc += k+", "
-    qc = qc[:-2]
-    qc += " WHERE \'lyrics\' IS NOT NULL"
-    return qc
+for p in all_tables:
+    pdf = pd.read_sql_query("SELECT * FROM \"" + p +"\"", DBhelper.engine)
+    lyrics = lyrics + list(pdf["lyrics"].dropna())
 
-
-qc = multipleTableNames(all_tables)
-print(qc)
-pdf = pd.read_sql_query(qc, DBhelper.engine)
-print(pdf.head(10))
-# lyrics = list(pdf["lyrics"])
-print("SHaboy", lyrics)
+print(lyrics)
 
 #### Form the right data structures
 from gensim import corpora
 
 documents = lyrics
-stoplist = set('for a of the and to in'.split())
-texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+stoplist = set('for a of the and to in yeah who don like got want know baby let hey come tell need'.split())
+# texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+
+# Use NLTK tokenizer to correctly split contracted words
+tokenizer = RegexpTokenizer(r'\w+')
+# Use NLTK stopwords to make sure common words are not being stored in list
+#stopset = set(stopwords.words('english'))
+stopset = set(spacy.en.STOP_WORDS)
+en_words = set(words.words())
+# texts = [tokenizer.tokenize(document) for document in documents]
+
+
+# Should I make the top N ranked words stopwords in the spacy dataset?
+def notStopWord(word):
+    if word not in stopset:
+        if word not in stoplist:
+            return word
+
+# Ensure words are lowercase
+texts = [list(map(lambda word: word.lower(), tokenizer.tokenize(document))) for document in documents]
+
+# Ensure words are not stopwords
+texts = [list(filter(notStopWord, document)) for document in texts]
+
+# Ensure words are at least 3 characters long
+texts = [list(filter(lambda word: len(word) >= 3, document)) for document in texts]
+
+# Ensure word is a real word in english dictionary
+texts = [list(filter(lambda word: word in en_words, document)) for document in texts]
+
+print(texts[1:2])
 
 # remove words that appear only once
 from collections import defaultdict
@@ -56,6 +79,7 @@ from pprint import pprint  # pretty-printer
 
 dictionary = corpora.Dictionary(texts)
 dictionary.save('/tmp/deerwester.dict')
+print("PRINTING DICTIONARY")
 print(dictionary)
 
 # print(dictionary.token2id)
@@ -78,17 +102,23 @@ corpus_tfidf = tfidf[corpus]
 # for doc in corpus_tfidf:
 #     print(doc)
 
-ntopics = 2
+ntopics = 10
 
 lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=ntopics)  # initialize an LSI transformation
 corpus_lsi = lsi[corpus_tfidf]  # create a double wrapper over the original corpus: bow->tfidf->fold-in-lsi
 
-lsi.print_topics(ntopics)
+print("Latent Semantic Indexing")
+for l in lsi.print_topics(ntopics):
+    print(l)
 
-for doc in corpus_lsi:  # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
-    print(doc)
+# for doc in corpus_lsi:  # both bow->tfidf and tfidf->lsi transformations are actually executed here, on the fly
+#     print(doc)
 
 # LDA
 lda_model = models.LdaModel(corpus, id2word=dictionary, num_topics=ntopics)
 
 lda_model.print_topics(ntopics)
+
+print("Latent Dirichlet Allocation")
+for l in lda_model.print_topics(ntopics):
+    print(l)
